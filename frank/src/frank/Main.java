04360092038b6e9beb3da092f160605c865b1800
João Marcos <jomamts@gmail.com>
2019-08-05 11:57:54 -0300

    adição das classes de impressão

--- frank/src/frank/Main.java
+++ frank/src/frank/Main.java
@@ -1,43 +1,30 @@
 package frank;
+import frank.parser.*;
 import frank.lexer.*;
 import frank.node.*;
 import java.io.*;
-import frank.parser.*;
 
 public class Main
 {
-	public static void main(String[] args)
-	{
-		try
-		{
-			String arquivo;
-//			if(args[0] != null) {
-//				arquivo = args[0];
-//			}
-			arquivo = "teste.frank";
+ public static void main(String[] args)
+ {
+  try
+  {
+   String arquivo = "teste/teste.frank";
+  
+   Parser p =
+    new Parser(
+    new Lexer(
+    new PushbackReader(  
+    new FileReader(arquivo), 1024))); 
+   
+   Start tree = p.parse();
 
-			Lexer lexer =
-					new Lexer(
-							new PushbackReader(  
-									new FileReader(arquivo), 1024)); 
-			
-			//Printer.printTokens(lexer);
-			
-			Parser parser = new Parser(lexer);
-			
-			Start start = parser.parse();
-			
-			System.out.println(start.toString());
-			
-			
-			//PDeclaracao pde = start.getPDeclaracao();
-			
-			
-			
-		}
-		catch(Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
-	}
+   tree.apply(new ASTDisplay());
+  }
+  catch(Exception e)
+  {
+   System.out.println(e.getMessage());
+  }
+ }
 }
